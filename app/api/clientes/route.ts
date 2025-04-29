ab9e85fccc9a6213a4d64c7561d82d4341729421
Gabriel Ximenes Mitozo Noronha <gabriel.noronha@grupotapajos.com.br>
2025-04-29 15:26:28 -0400

    feat:fixing libs

--- app/api/clientes/route.ts
+++ app/api/clientes/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
+import prisma from '@/prisma';
 
-import { Client } from '@/types';
-import { prisma } from '@/lib/prisma';
 
 export async function GET() {
   try {
