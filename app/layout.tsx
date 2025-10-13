f70334cb40267a70842a1a390f31cd11522f8ddc
gnoronha42 <gabrielximenesnoronha@gmail.com>
2025-10-13 11:12:30 -0400

    feat:add calculator

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,15 +17,16 @@ export default function RootLayout({
   const isLoginPage = pathname === "/login";
   const isSelleriaPage = pathname === "/selleria";
   const isObrigadoPage = pathname === "/obrigado";
+  const isCalculadoraPage = pathname === "/calculadora";
 
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
           <div className="min-h-screen">
-            {!isLoginPage && !isSelleriaPage && !isObrigadoPage && <Sidebar />}
-            <main className={`${!isLoginPage && !isSelleriaPage && !isObrigadoPage   ? 'md:pl-80 p-4 md:p-6' : ''} min-h-screen bg-gray-50 dark:bg-zinc-900`}>
-              <div className={`${!isLoginPage && !isSelleriaPage && !isObrigadoPage  ? 'max-w-7xl mx-auto' : ''}`}>
+            {!isLoginPage && !isSelleriaPage && !isObrigadoPage && !isCalculadoraPage && <Sidebar />}
+            <main className={`${!isLoginPage && !isSelleriaPage && !isObrigadoPage && !isCalculadoraPage ? 'md:pl-80 p-4 md:p-6' : ''} min-h-screen bg-gray-50 dark:bg-zinc-900`}>
+              <div className={`${!isLoginPage && !isSelleriaPage && !isObrigadoPage && !isCalculadoraPage ? 'max-w-7xl mx-auto' : ''}`}>
                 {children}
               </div>
             </main>
